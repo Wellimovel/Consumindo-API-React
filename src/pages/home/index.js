77ccc7a2f88d5440601d41e6df96ecb0cb0f640f
Wellington <wellington@wellimovel.com>
2023-12-07 12:53:39 -0300

    atualização 1

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 import {Header} from '../../components/Header';
 import background from '../../assets/background.png';
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <div classname="conteudo">
-      <img src={background} classname="background" alt="background app"></img>
+      <img src={background} className="background" alt="background app"></img>
+      
     </div>
     </div>
   );
